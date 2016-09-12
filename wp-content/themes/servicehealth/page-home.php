628f43128044a40c5f7b43e8633b15b679d6d81a
Dan Shoreman <danshoreman@Dans-MBP.home>
2016-09-12 22:40:04 +0100

    Adding static markup

--- wp-content/themes/servicehealth/page-home.php
+++ wp-content/themes/servicehealth/page-home.php
@@ -1,67 +1,203 @@
 <?php get_header(); ?>
 
-	<div class="row">
-	
-		<div class="small-12 medium-8 columns" role="main">
-
-			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> role="article">
-				
-				<?php if ( has_post_thumbnail()) : ?>
-						
-					<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" class="post-thumbnail">
-					
-						<?php the_post_thumbnail(); ?>
-					
-					</a>
-				
-				<?php endif; ?>
-
-				<header class="article-header">
-
-					<h1 itemprop="headline"><?php the_title(); ?></h1>
-
-				</header>
-
-				<section class="entry-content" itemprop="articleBody">
-					
-					<?php the_content(); ?>
-
-				</section>
-
-				<?php comments_template(); ?>
-
-			</article>
-
-			<?php endwhile; else : ?>
-
-			<article class="post-not-found">
-			
-				<header class="not-found-header">
-					
-					<h2><?php _e( 'Nothing Found!' ); ?></h2>
-				
-				</header>
-				
-				<section class="not-found-content">
-					
-					<p><?php _e( 'Please check what you are looking for.' ); ?></p>
-				
-				</section>
-			
-			</article>
-
-			<?php endif; ?>
-
-		</div>
-
-		<div class="small-12 medium-4 columns">
+	<section class="panel hero--panel">
 		
-			<?php get_sidebar(); ?>
+	</section>
+	
+	<section class="panel intro--panel">
+		<h2>Urgent. Business-critical.<br>
+		Really flipping important.</h2>
+		
+		<div class="row">
+			<div class="small-12 medium-6 columns">
+				<img src="http://placehold.it/450x300" class="" alt="">
+			</div>
+			<div class="small-12 medium-6 columns">
+				<p>Whether it’s breaking news – or breaking servers – some messages are too important to be ignored.<br>
+					And yet somehow they are. 
+				</p>
+				
+			</div>
+		</div>
+		
+		<p>When everyone’s inbox has a mind-bogglingly high ‘unread message’ count, the humble email won’t cut it anymore. Messages get missed, people get miffed, and business grinds to a halt.</p>
+		
+		<h3>But there’s an answer! And it’s a clever one!</h3>
+		
+		<div class="">
+			<div class="row">
+				<div class="small-12 medium-4 columns">
+					<p>Service Health is a one-stop shop to send instant notifications to anyone in your business. Wherever they are, whatever platform they’re using.</p>
+				</div>	
+				<div class="small-12 medium-4 columns">
+					<p>In fact, you can even let them choose how they’d like to be notified, and which notifications are relevant to them.</p>
+				</div>
+				<div class="small-12 medium-4 columns">
+					<p>So if Joan from HR is going to be on the road on Thursday, she can choose to get useful HR alerts straight to her mobile. It’s democracy in action.</p>
+				</div>		
+			</div>
+		</div>
+		
+		<p>You get happier staff, your alerts actually get read, and nobody gets any nasty surprises.</p>
+		
+		<p>Click play below to see how Service Health can save you time, money and sweet, sweet sanity.</p>
+		
+		<a href="" class="btn btn--pricing">View pricing</a>
+		
+		<a href="" class="btn btn--video">Watch the video</a>
 			
 		</div>
+		
+	</section>
+	
+	<section class="panel panel--full--width">
+		
+	</section>
+	
+	<section class="panel panel--features">
+		<header class="panel-header">
+			<h3>The Things you’ll love</h3>
+			<p>As if reducing service desk calls and giving you the time to actually fix the issue wasn’t enough...</p>
+		</header>
+		
+		<div class="row">
+			<div class="small-12 medium-6 columns">
+				<img src="http://placehold.it/450x300" class="" alt="">
+			</div>
+			<div class="small-12 medium-6 columns">
+				<div class="block feature--block">
+					<h4>It’s in the cloud – and at your fingertips</h4>
+					<p>Your real-time dashboard is only ever a click away. You can check on services 24/7 and see how your communication’s performing with notification tracking. It’s seriously easy</p>
+				</div>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="small-12 medium-6 columns">
+				<div class="block feature--block">
+					<h4>Location-specific alerting? Oh yes</h4>
+					<p>However many sites you have – and wherever they are in the world – you can send targeted alerts by location. Service Health gets attention like a foghorn, but it’s accurate like a scalpel.</p>
+				</div>
+			</div>
+			<div class="small-12 medium-6 columns">
+				<img src="http://placehold.it/450x300" class="" alt="">
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="small-12 medium-6 columns">
+				<img src="http://placehold.it/450x300" class="" alt="">
+			</div>
+			<div class="small-12 medium-6 columns">
+				<div class="block feature--block">
+					<h4>A problem shared is a problem halved</h4>
+					<p>When your users are kept in the dark they panic. People get distracted, annoyed, and they take up valuable IT time by reporting issues. For the thousandth time. A quick alert from Service Health minimises confusion, downtime and cost to the business.</p>
+				</div>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="small-12 medium-6 columns">
+				<div class="block feature--block">
+					<h4>It’s on your users’ terms</h4>
+					<p>Since users choose when and where they receive alerts, they’ll actually be heard. If email’s not good for someone, or they’d like to get SMS alerts after business hours, the choice is in their hands. Or on their desktops. Or both, if you’re the belt-and-braces type.</p>
+				</div>
+			</div>
+			<div class="small-12 medium-6 columns">
+				<img src="http://placehold.it/450x300" class="" alt="">
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="small-12 medium-6 columns">
+				<img src="http://placehold.it/450x300" class="" alt="">
+			</div>
+			<div class="small-12 medium-6 columns">
+				<div class="block feature--block">
+					<h4>It’s not just for the techie types</h4>
+					<p>Whether it’s HR updates, a staff newsletter, or giving someone a good old pat on the back for a job well done, Service Health lets people know.<br><br>
 
-	</div>
+ People are already coming up with uses we never imagined, and they’re making their businesses a better place to be.</p>
+				</div>
+			</div>
+		</div>
+		
+	</section>
+	
+	<section class="panel panel--examples">
+		<div class="row">
+			<div class="small-12 columns">
+				
+				<ul class="carousel">
+					<li>
+						<img src="http://placehold.it/250x150" class="" alt="">
+						<h5>Example 01</h5>
+						<p>Email is down at our London Park Gate office. This is the result of a BT issue – we are investigating and will update you as soon as we can.</p></li>
+					<li>
+						<img src="http://placehold.it/250x150" class="" alt="">
+						<h5>Example 02</h5>
+						<p>Email is down at our London Park Gate office. This is the result of a BT issue – we are investigating and will update you as soon as we can.</p></li>
+					<li>
+						<img src="http://placehold.it/250x150" class="" alt="">
+						<h5>Example 03</h5>
+						<p>Email is down at our London Park Gate office. This is the result of a BT issue – we are investigating and will update you as soon as we can.</p></li>
+				</ul>
+				
+			</div>
+		</div>
+	</section>
+	
+	<section class="panel panel--pricing">
+		<h4>Pricing</h4>
+		
+		<div class="row">
+			<div class="small-12 medium-6 columns">
+				<div class="block block--pricing">
+					<h6>Free Trial</h6>
+					
+					<p>Up to 3 users<br> + Creator Licence</p>
+					
+					<p>Free – 10 email and 10 SMS<br>text messages.</p>
+					
+					<p>Additional SMS messages invoiced monthly in arrears @ 0.033pence per message sent.</p>
+					
+					<a href="" class="btn btn-pricing">Enquire Now</a>
+				</div>
+				<div class="block block--pricing">
+					<h6>Team</h6>
+					
+					<p>Up to 50 users<br> + Creator Licence</p>
+					
+					<p>£2.00 Per Seat Per Month – Text, SMS + Windows Pop Up.<br></p>
+					
+					<p>Additional SMS messages invoiced monthly in arrears @ 0.033pence per message sent.</p>
+					
+					<a href="" class="btn btn-pricing">Enquire Now</a>
+				</div>
+				<div class="block block--pricing">
+					<h6>Business</h6>
+					
+					<p>Up to 3 users<br> + Creator Licence</p>
+					
+					<p>Licence - £1.50 Per Seat Per Month<br>Text, SMS + Windows Pop Up.</p>
+					
+					<p>Additional SMS messages invoiced monthly in arrears @ 0.033pence per message sent.</p>
+					
+					<a href="" class="btn btn-pricing">Enquire Now</a>
+				</div>
+				<div class="block block--pricing">
+					<h6>Enterprise</h6>
+					
+					<p>Up to 3 users<br> + Creator Licence</p>
+					
+					<p>Free – 10 email and 10 SMS<br>text messages.</p>
+					
+					<p>Additional SMS messages invoiced monthly in arrears @ 0.033pence per message sent.</p>
+					
+					<a href="" class="btn btn-pricing">Enquire Now</a>
+				</div>
+			</div>
+		</div>
+	</section>
 
 <?php get_footer(); ?>
